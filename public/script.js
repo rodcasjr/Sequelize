bc72c9d171fa2c21ac434f69e4220dd2ce4365af
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-03-26 09:19:30 -0400

    update table

--- public/script.js
+++ public/script.js
@@ -1,15 +1,42 @@
 async function getData() {
-  console.log('data request');
+  // console.log('data request');
   const dining = document.querySelector('#dining');
-  const request = await fetch('http://localhost:3000/api/dining', {method: 'GET', mode: 'no-cors'});
+  const request = await fetch('http://localhost:3000/api/dining', {method: 'GET'});
+  console.log(request);
   const tableData = await request.json();
-  return tableData;
+  console.table(tableData.data);
 
-  // window.addEventListener('load', (event) => {
-  //   console.log('page loaded');
-  // });
-  const appendItem = document.createElement('td');
-  appendItem.classList.add('dining');
-}
+  tableData.data.forEach((restaurant) => {
+    const appendItem = document.createElement('div');
+    // appendItem.classList.add('title', 'has-text-centered', 'is-parent', 'is-3');
+    appendItem.innerHTML =
+    `<tr>${restaurant.hall_id}</tr>
+      <br/>
+      <tr>${restaurant.hall_name}</tr>
+      <br/>
+      <tr>${restaurant.hall_address}</tr>`;
+  }).join('');
+    dining.innerHTML = html;
+    // targetBox.append(appendItem);
+};
+
+
+
+//   function addData() {
+//     const hallId = document.getElementById('hallId').value;
+//     const hallName = document.getElementById('hallName').value;
+//     const hallAddress = document.getElementById('hallAddress').value;
+
+//     let html = ' ';
+//     html = `<tr><td>${hallId.value.data}</td><td>${hallName.value.data}</td><td>${hallAddress.value.data}</td></tr>`;
+
+//     document.getElementById('result').innerHTML += html;
+
+//     document.getElementById('hallId').value = '';
+//     document.getElementById('hallName').value = '';
+//     document.getElementById('hallAddress').value = '';
+//   }
+//   window.onload = addData;
+// }
 
 window.onload = getData;
