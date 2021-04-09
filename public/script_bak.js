005907bdf37e470bb0a5b0baf18a7a85fdda57df
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-04-08 21:23:58 -0400

    backup

---
+++ public/script_bak.js
@@ -0,0 +1,30 @@
+async function getData() {
+  console.log('data request');
+  const result = document.querySelector('#result');
+  const request = await fetch('/api/dining');
+  const tableData = await request.json();
+  // return tableData;
+
+  tableData.data.forEach((restaurant) => {
+    console.log(restaurant);
+    const appendItem = document.createElement('tr');
+    // appendItem.classList.add('title', 'has-text-centered', 'is-parent', 'is-3');
+    appendItem.innerHTML = `
+      <td>
+        ${restaurant.hall_id}
+      </td>
+      
+      <td>
+        ${restaurant.hall_name}
+      </td>
+      
+      <td>
+      ${restaurant.hall_address}
+      </td>`;
+    result.append(appendItem);
+  });
+}
+window.onload = getData;
+// class="has-text-light"
+// class="title is-child box has-background-link-dark"
+// class="subtitle has-text-light has-text-weight-bold"
